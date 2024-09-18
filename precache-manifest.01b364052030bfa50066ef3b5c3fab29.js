b18a3d1bc8c73374f5317f56d18b882148829c95
bonnienunes <bonnieives@gmail.com>
2024-09-18 18:59:34 -0400

    Updates

---
+++ precache-manifest.01b364052030bfa50066ef3b5c3fab29.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c7c9c62d2cbef5a7324260158d6793",
+    "url": "/sicma-website/index.html"
+  },
+  {
+    "revision": "ff186d052b2b29e7e9cc",
+    "url": "/sicma-website/static/css/2.9bd6c941.chunk.css"
+  },
+  {
+    "revision": "2db19744740bf3d059fe",
+    "url": "/sicma-website/static/css/main.e74dca94.chunk.css"
+  },
+  {
+    "revision": "ff186d052b2b29e7e9cc",
+    "url": "/sicma-website/static/js/2.382d84c8.chunk.js"
+  },
+  {
+    "revision": "8e77598a89468bbb272f9d779b136c11",
+    "url": "/sicma-website/static/js/2.382d84c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b5e41e9afe766cf522e",
+    "url": "/sicma-website/static/js/3.c2db4683.chunk.js"
+  },
+  {
+    "revision": "2db19744740bf3d059fe",
+    "url": "/sicma-website/static/js/main.4d41aaa3.chunk.js"
+  },
+  {
+    "revision": "bdaa6ed013ee9340ba06",
+    "url": "/sicma-website/static/js/runtime-main.148534be.js"
+  },
+  {
+    "revision": "37f3a56cf094402b44d1329ed8f1f609",
+    "url": "/sicma-website/static/media/Ethics-renewables.37f3a56c.webp"
+  },
+  {
+    "revision": "04c38a99d8afcbb092591a7a88a39a88",
+    "url": "/sicma-website/static/media/Kollektif-Bold.04c38a99.ttf"
+  },
+  {
+    "revision": "4d49be6e42d18464b230d87d1fab175a",
+    "url": "/sicma-website/static/media/Kollektif.4d49be6e.ttf"
+  },
+  {
+    "revision": "80b57904279753a1b84b06f80390d8e7",
+    "url": "/sicma-website/static/media/R.80b57904.jpg"
+  },
+  {
+    "revision": "0631cb9f8b98e59cea272d6e472d0e2e",
+    "url": "/sicma-website/static/media/elderly-person-children-holding-plant.0631cb9f.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/sicma-website/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3a943ce10c6e881f1a0abb8831c63ded",
+    "url": "/sicma-website/static/media/pexels-obviouslyarthur-1296265.3a943ce1.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/sicma-website/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/sicma-website/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/sicma-website/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "6cd54c0faebdb555af65867a2a132832",
+    "url": "/sicma-website/static/media/tree-5102896_1280.6cd54c0f.jpg"
+  }
+]);
